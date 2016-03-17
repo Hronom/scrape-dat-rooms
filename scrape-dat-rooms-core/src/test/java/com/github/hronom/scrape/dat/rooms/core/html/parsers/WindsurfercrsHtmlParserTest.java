a8611b1e2fce709b1703e66ce9ddd603b566ed08
Hronom <hronom@gmail.com>
2016-03-18 00:47:59 +0200

    Add Windsurfercrs HtmlParser test

---
+++ scrape-dat-rooms-core/src/test/java/com/github/hronom/scrape/dat/rooms/core/html/parsers/WindsurfercrsHtmlParserTest.java
@@ -0,0 +1,102 @@
+package com.github.hronom.scrape.dat.rooms.core.html.parsers;
+
+import com.github.hronom.scrape.dat.rooms.core.html.parsers.utils.NetworkUtils;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertNull;
+
+public class WindsurfercrsHtmlParserTest {
+    private WindsurfercrsHtmlParser parser;
+
+    @Before
+    public void setUp() throws Exception {
+        parser = new WindsurfercrsHtmlParser();
+    }
+
+    @Test
+    public void testParse() throws Exception {
+        String inputHtml = new Scanner(this.getClass().getResourceAsStream("Windsurfercrs_1.html"),
+            StandardCharsets.UTF_8.toString()
+        ).useDelimiter("\\Z").next();
+
+        ArrayList<RoomInfo> roomInfos = parser.parse(inputHtml, new RoomPhotoDownloader() {
+            @Override
+            public Path download(String url) {
+                return NetworkUtils.srcImageToSavePath(url, Paths.get(""));
+            }
+        });
+
+        Iterator<RoomInfo> iter = roomInfos.iterator();
+
+        // 1.
+        assertTrue(iter.hasNext());
+        {
+            RoomInfo roomInfo = iter.next();
+            assertEquals("ShergillGrandHotelCityViewsmall.jpg", roomInfo.roomPhotoPath);
+            assertEquals("143.65$", roomInfo.rate);
+            assertNull(roomInfo.description);
+            assertEquals("2 Queens City View", roomInfo.amenities);
+        }
+
+        // 2.
+        assertTrue(iter.hasNext());
+        {
+            RoomInfo roomInfo = iter.next();
+            assertEquals("ShergillGrandHotelPVsmall.jpg", roomInfo.roomPhotoPath);
+            assertEquals("152.15$", roomInfo.rate);
+            assertNull(roomInfo.description);
+            assertEquals("2 Queens Poolview", roomInfo.amenities);
+        }
+
+        // 3.
+        assertTrue(iter.hasNext());
+        {
+            RoomInfo roomInfo = iter.next();
+            assertEquals("ShergillGrandHotelEQ1small.jpg", roomInfo.roomPhotoPath);
+            assertEquals("152.15$", roomInfo.rate);
+            assertNull(roomInfo.description);
+            assertEquals("1 Queen Tower Room", roomInfo.amenities);
+        }
+
+        // 4.
+        assertTrue(iter.hasNext());
+        {
+            RoomInfo roomInfo = iter.next();
+            assertEquals("ShergillGrandHotelKingRoomEK.jpg", roomInfo.roomPhotoPath);
+            assertEquals("160.65$", roomInfo.rate);
+            assertNull(roomInfo.description);
+            assertEquals("1 King Tower Room", roomInfo.amenities);
+        }
+
+        // 5.
+        assertTrue(iter.hasNext());
+        {
+            RoomInfo roomInfo = iter.next();
+            assertEquals("ShergillGrandHotelEQQsmall.jpg", roomInfo.roomPhotoPath);
+            assertEquals("160.65$", roomInfo.rate);
+            assertNull(roomInfo.description);
+            assertEquals("2 Queens Tower Room", roomInfo.amenities);
+        }
+
+        // 6.
+        assertTrue(iter.hasNext());
+        {
+            RoomInfo roomInfo = iter.next();
+            assertEquals("ShergillGrandHotelEKJsmall.jpg", roomInfo.roomPhotoPath);
+            assertEquals("169.15$", roomInfo.rate);
+            assertNull(roomInfo.description);
+            assertEquals("King Jacuzzi Tower Room", roomInfo.amenities);
+        }
+    }
+}
